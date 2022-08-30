7b944ac6f12b7f5545b8c3207d36dba114741cbf
AustinArrell <austinarrell@outlook.com>
2022-08-29 21:50:39 -0700

    Added Genre Index View

---
+++ LiteratureLounge/Controllers/GenreController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LiteratureLounge.Models;
+using System.Diagnostics;
+using Purrs_And_Prose.Data;
+
+namespace LiteratureLounge.Controllers
+{
+    public class GenreController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _db;
+
+        public GenreController(ApplicationDbContext db, ILogger<HomeController> logger)
+        {
+            _db = db;
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Genre> genres = _db.Genres.ToList();
+            return View(genres);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
