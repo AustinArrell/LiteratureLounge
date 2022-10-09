503f39c20f7aef41f4dbefeca75654d8d255b6d8
Austin Arrell <austinarrell@gmail.com>
2022-10-08 17:46:12 -0700

    Removed required tag from ISBN

--- LiteratureLounge/Models/Book.cs
+++ LiteratureLounge/Models/Book.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Newtonsoft.Json.Serialization;
+using System.ComponentModel.DataAnnotations;
 
 namespace LiteratureLounge.Models
 {
@@ -7,11 +8,10 @@ namespace LiteratureLounge.Models
         [Key]
         public int Id { get; set; }
         [Required]
-        public string? Title { get; set; }
+        public string Title { get; set; }
         public string? Subtitle { get; set; }
         [Required]
-        public string? Author { get; set; }
-        [Required]
+        public string Author { get; set; }
         public string? ISBN { get; set; }
         public string? Publisher { get; set; }
         public string? Description { get; set; }
--- LiteratureLounge/Models/BookEditViewModel.cs
+++ LiteratureLounge/Models/BookEditViewModel.cs
@@ -4,7 +4,6 @@
     {
         public Book book { get; set; }
         public string genreName { get; set; }
-
         public List<string> SeriesNames { get; set; } = new List<string>();
         public List<string> genreNames { get; set; } = new List<string>();
         public IEnumerable<Genre> Genres { get; set; } = new List<Genre>();
