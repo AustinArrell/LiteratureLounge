d69780ad2746919789393a3486d62aaf6c5f7777
Austin Arrell <austinarrell@outlook.com>
2022-10-12 20:04:19 -0700

    Added the nullable flag to the Owner field

---
+++ LiteratureLounge/Migrations/20221013030147_MakeOwnerNullable.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LiteratureLounge.Migrations
+{
+    public partial class MakeOwnerNullable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Owner",
+                table: "Books",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Owner",
+                keyValue: null,
+                column: "Owner",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Owner",
+                table: "Books",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
