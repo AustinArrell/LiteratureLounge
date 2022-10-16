997ec87d4d114f2fd1c5a8f57bce6b71b7686532
Austin Arrell <austinarrell@outlook.com>
2022-10-15 18:25:38 -0700

    Setup navbar login buttons to adjust based on if a user is logged in

--- LiteratureLounge/Controllers/AccountController.cs
+++ LiteratureLounge/Controllers/AccountController.cs
@@ -1,52 +1,58 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using LiteratureLounge.Models;
-using System.Diagnostics;
-using Purrs_And_Prose.Data;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Auth0.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication;
-using System.Security.Claims;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
-namespace LiteratureLounge.Controllers
+public class AccountController : Controller
 {
-    public class AccountController : Controller
+    public async Task Login(string returnUrl = "/")
     {
-        private readonly ILogger<HomeController> _logger;
-        private readonly ApplicationDbContext _db;
+        var authenticationProperties = new LoginAuthenticationPropertiesBuilder()
+          // Indicate here where Auth0 should redirect the user after a login.
+          // Note that the resulting absolute Uri must be added to the
+          // **Allowed Callback URLs** settings for the app.
+          .WithRedirectUri(Url.Action("Index", "Book"))
+          .Build();
 
-        public AccountController(ApplicationDbContext db, ILogger<HomeController> logger)
+        await HttpContext.ChallengeAsync(
+          Auth0Constants.AuthenticationScheme,
+          authenticationProperties
+        );
+    }
+
+    [Authorize]
+    public IActionResult Profile()
+    {
+        return View(new
         {
-            _db = db;
-            _logger = logger;
-        }
+            Name = User.Identity.Name,
+            EmailAddress = User.Claims
+            .FirstOrDefault(c => c.Type == ClaimTypes.Email)?.Value,
+            ProfileImage = User.Claims
+            .FirstOrDefault(c => c.Type == "picture")?.Value
+        });
+    }
 
-        public async Task Login(string returnUrl = "/")
-        {
-            var authenticationProperties = new LoginAuthenticationPropertiesBuilder()
-                // Indicate here where Auth0 should redirect the user after a login.
-                // Note that the resulting absolute Uri must be added to the
-                // **Allowed Callback URLs** settings for the app.
-                .WithRedirectUri(returnUrl)
-                .Build();
+    [Authorize]
+    public async Task Logout()
+    {
+        var authenticationProperties = new LogoutAuthenticationPropertiesBuilder()
+          // Indicate here where Auth0 should redirect the user after a logout.
+          // Note that the resulting absolute Uri must be added to the
+          // **Allowed Logout URLs** settings for the app.
+          .WithRedirectUri(Url.Action("Index", "Home"))
+          .Build();
 
-            await HttpContext.ChallengeAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);
-        }
-
-        [Authorize]
-        public IActionResult Profile()
-        {
-            return View(new
-            {
-                Name = User.Identity.Name,
-                EmailAddress = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Email)?.Value,
-                ProfileImage = User.Claims.FirstOrDefault(c => c.Type == "picture")?.Value
-            });
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        // Logout from Auth0
+        await HttpContext.SignOutAsync(
+          Auth0Constants.AuthenticationScheme,
+          authenticationProperties
+        );
+        // Logout from the application
+        await HttpContext.SignOutAsync(
+          CookieAuthenticationDefaults.AuthenticationScheme
+        );
     }
 }
