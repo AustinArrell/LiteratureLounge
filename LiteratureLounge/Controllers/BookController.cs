53915be14d081a61afbf508a02d3284c23c4efd4
Austin Arrell <austinarrell@outlook.com>
2022-10-15 21:21:15 -0700

    Added userId filter to Delete action

--- LiteratureLounge/Controllers/BookController.cs
+++ LiteratureLounge/Controllers/BookController.cs
@@ -276,10 +276,11 @@ namespace LiteratureLounge.Controllers
         [Authorize]
         public IActionResult Delete(int? id)
         {
-            var book = _db.Books.FirstOrDefault(c => c.Id == id);
+            var userId = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
+            var book = _db.Books.Where(b => b.Owner == userId).FirstOrDefault(c => c.Id == id);
             if (book == null)
             {
-                TempData["Error"] = "Failed to remove book!";
+                TempData["Error"] = $"Action Failed! Cannot find book!";
                 return RedirectToAction("Index");
             }
             return View(book);
