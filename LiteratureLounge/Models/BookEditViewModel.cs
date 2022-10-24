9542a484c35bcb47cd91002544a00b94d0cd0cff
Austin Arrell <austinarrell@outlook.com>
2022-10-23 17:19:23 -0700

    Added Title and Author fields to viewModel. Changed view to validate on viewmodel rather than model

--- LiteratureLounge/Models/BookEditViewModel.cs
+++ LiteratureLounge/Models/BookEditViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace LiteratureLounge.Models
 {
@@ -7,7 +9,13 @@ namespace LiteratureLounge.Models
         public Book book { get; set; }
         public List<string> SeriesNames { get; set; } = new List<string>();
         public List<int> genreIds { get; set; } = new List<int>();
-
         public MultiSelectList GenreSelectItems { get; set; }
+
+        [DisplayName("Title")]
+        [Required(ErrorMessage = "Please enter Title")]
+        public string Title { get; set; }
+        [DisplayName("Author")]
+        [Required(ErrorMessage = "Please enter Author")]
+        public string Author { get; set; }
     }
 }
